3e88cbedfaabf5906700103951864fb28a822564
dariram <104618523+dariram@users.noreply.github.com>
2023-10-01 13:05:02 +0530

    Lunchtime
    Chef has his lunch only between 
    1
    1 pm and 
    4
    4 pm (both inclusive).
    
    Given that the current time is 
    
    X pm, find out whether it is lunchtime for Chef.
    
    Input Format
    The first line of input will contain a single integer 
    �
    T, the number of test cases. Then the test cases follow.
    Each test case contains a single line of input, containing one integer 
    �
    X.
    Output Format
    For each test case, print in a single line 
    YES
    YES if it is lunchtime for Chef. Otherwise, print 
    NO
    NO.
    
    You may print each character of the string in either uppercase or lowercase (for example, the strings 
    YeS
    YeS, 
    yEs
    yEs, 
    yes
    yes and 
    YES
    YES will all be treated as identical).

---
+++ LTIME.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+int ik;
+cin>>ik;
+for(int i=0;i<ik;i++){
+	int take;
+	cin>>take;
+	if((take>=1)&&(take<=4)){
+	    cout<<"Yes"<<endl;
+	}
+	else{
+	    cout<<"No"<<endl;
+	}
+	
+}}
